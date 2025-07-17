af352bfe05c82305428a848f57d400304d9bc687
yukta <yuktaa@cybage.com>
2025-07-17 13:56:49 +0530

    fg

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -2,7 +2,7 @@ resource "google_project_iam_member" "gke_sa_roles" {
   for_each = toset(var.roles)
   project  = var.project_id
   role     = each.key
-  member   = "serviceAccount:${google_service_account.gke_sa.email}"
+  member   = "serviceAccount:${var.account_id}"
 }
 
 output "service_account_email" {
