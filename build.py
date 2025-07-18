b934232316bc1c888cbf934cd1a89c54f1bc213d
yukta <yuktaa@cybage.com>
2025-07-18 16:23:11 +0530

    l

--- build.py
+++ build.py
@@ -3,8 +3,7 @@ from pybuilder.core import use_plugin, init
 use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
-use_plugin("python.pytest")
-use_plugin("python.pytest_coverage")
+use_plugin("python.coverage")
 
 name = "cursor-app"
 default_task = ["analyze", "run_unit_tests"]
@@ -14,5 +13,5 @@ def set_properties(project):
     project.build_depends_on("Flask")
     project.build_depends_on("pytest")
     project.build_depends_on("pytest-cov")
-    project.set_property("pytest_coverage_break_build", False)
-    project.set_property("pytest_extra_args", ["--cov=.", "--cov-report=xml"]) 
+    project.set_property("coverage_break_build", False)
+    project.set_property("coverage_threshold_warn", 0) 
